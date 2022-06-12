f5962f2165e0b807a754d9b130b7086a2642c1f3
jewonyeon <yjw9852@gmail.com>
2022-06-13 01:21:26 +0900

    버블정렬

---
+++ sort.js
@@ -0,0 +1,15 @@
+function sort_bubble(array) {
+  const length = array.length;
+  let temp;
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = 0; j < length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+};
+
